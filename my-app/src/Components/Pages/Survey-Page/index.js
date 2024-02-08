407e8ff8e0628584367ff6ecacf7743dfdb37a4a
Folawe Rahman <Rahmanfolawe@gmail.com>
2024-02-08 21:51:49 +0100

    Feat- Survey Page

---
+++ my-app/src/Components/Pages/Survey-Page/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+
+export const Survey = () => {
+
+        return(
+            <div>
+                
+                <div>
+                    <h4>Welcome to our Career Path Recommendation Survey!</h4>
+                    <p>This brief questionnaire will help you explore your interests and skills, providing personalized guidance on which path aligns better with your aspirations.</p>
+                </div>
+
+
+
+
+
+
+
+
+
+            </div>
+
+
+
+        )
+
+
+}
